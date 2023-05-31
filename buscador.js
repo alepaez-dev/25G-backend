b8fddaa6469a8eec57f1794a5425db8793640938
Alejandra Paez <alepaezdev@gmail.com>
2023-05-30 20:48:06 -0600

    feat: info

--- buscador.js
+++ buscador.js
@@ -21,7 +21,8 @@ const nombres = [
   "Samantha"
 ]
 /**
- * Pasarle un nombre a nuestro proceso de node, y verificar si ese nombre existe en nuestro arreglo de Nombres.
+ * Pasarle un nombre a nuestro proceso de node(recibir parametros desde consola), 
+ * y verificar si ese nombre existe en nuestro arreglo de Nombres.
  * Si existe imprimir `Bienvenido nombre`. Sino, imprimir `Fuera de aqui`.
  * 
  * Esto tiene que estar en una funcion.
